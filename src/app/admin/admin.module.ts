1b4910974f7e70ec2a024e110a75f3f0978adabe
Alexander Kulikov <bullet_310@mail.ru>
2020-02-10 00:25:28 +0300

    create getAll posts & pipe search

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,10 +8,10 @@ import { LoginPageComponent } from "./login-page/login-page.component";
 import { DashboardPageComponent } from "./dashboard-page/dashboard-page.component";
 import { CreatePageComponent } from "./create-page/create-page.component";
 import { EditPageComponent } from "./edit-page/edit-page.component";
-import { AuthService } from "./shared/services/auth.service";
 import { SharedModule } from "../shared/shared.module";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { AuthGuard } from "./shared/services/auth.guard";
+import { SearchPipe } from "./shared/search.pipe";
 
 @NgModule({
   declarations: [
@@ -19,7 +19,8 @@ import { AuthGuard } from "./shared/services/auth.guard";
     LoginPageComponent,
     DashboardPageComponent,
     CreatePageComponent,
-    EditPageComponent
+    EditPageComponent,
+    SearchPipe
   ],
   imports: [
     CommonModule,
--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { PostsService } from "src/app/shared/posts.service";
+import { Post } from "src/app/shared/interfaces";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-dashboard-page',
-  templateUrl: './dashboard-page.component.html',
-  styleUrls: ['./dashboard-page.component.scss']
+  selector: "app-dashboard-page",
+  templateUrl: "./dashboard-page.component.html",
+  styleUrls: ["./dashboard-page.component.scss"]
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  pSub: Subscription;
+  searchStr = "";
+  constructor(private postService: PostsService) {}
 
-  constructor() { }
+  remove(id: string) {}
 
   ngOnInit() {
+    this.pSub = this.postService.getAll().subscribe(posts => {
+      this.posts = posts;
+    });
   }
-
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
+  }
 }
